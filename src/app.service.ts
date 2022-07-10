ea9c357df69c6afe79d15df625cc360fbf540195
Jin Hyeok Son <thswlsgur98@gmail.com>
2022-07-10 20:13:23 +0900

    feat: prisma usage demo

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'nestjs-prisma';
 
 @Injectable()
 export class AppService {
+  constructor(private prismaService: PrismaService) {}
   getHello(): string {
+    this.prismaService.user.create({ data: { email: 'email', name: 'name' } });
     return 'Hello World!';
   }
 }
